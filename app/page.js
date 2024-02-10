a4c9cf407774e54cbdcfa2d6e03944ad76ddf299
Prince Asiedu <princekofasiedu@gmail.com>
2024-02-10 12:04:09 +0000

    feature: updated nav bar

--- app/page.js
+++ app/page.js
@@ -1,24 +1,17 @@
 import Link from "next/link";
+import { ListPlus } from "lucide-react";
 import { Logo } from "./components/common/ui/Logo";
 export default function Home() {
   return (
     <section className="bg-black h-full">
       <header className="px-[40px] bg-black">
-        <nav className="h-[120px] py-8 flex items-center gap-5">
-          <div className="w-[80px] h-[50px] ">
+        <nav className="h-[80px] py-8 flex items-center gap-5">
+          <div className="w-[80px] h-[70px] ">
             <Logo />
           </div>
-          <button className="text-white font-medium flex gap-3 w-[120px] h-[40px] bg-blue-500 ">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="48"
-              height="48"
-              id="playlist"
-            >
-              <path fill="#fff" d="M0 0h48v48H0z"></path>
-              <path d="M28 20H4v4h24v-4zm0-8H4v4h24v-4zm8 16v-8h-4v8h-8v4h8v8h4v-8h8v-4h-8zM4 32h16v-4H4v4z"></path>
-            </svg>
-            Playlists
+          <button className="text-white text-sm font-medium flex items-center px-2 gap-2 w-[130px] h-[30px] bg-blue-500 rounded-md ">
+            <ListPlus />
+            My Playlists
           </button>
         </nav>
       </header>
