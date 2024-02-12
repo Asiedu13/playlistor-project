7f8578d38dd01afd73e89c8d3fbd4256cb52e010
Prince Asiedu <princekofasiedu@gmail.com>
2024-02-12 23:08:43 +0000

    style: updated colour scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        'secondary': 'var(--secondary)',
+        'background': 'var(--background)'
+      }
     },
   },
   plugins: [],
