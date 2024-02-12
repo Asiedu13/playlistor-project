1bf4567344af0c461d76ba81544c6cf58bbf5b50
Prince Asiedu <princekofasiedu@gmail.com>
2024-02-12 23:09:53 +0000

    package: radix UI setup

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "@radix-ui/themes/styles.css";
 
+import { Theme } from "@radix-ui/themes";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -12,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <Theme>{children}</Theme>
       </body>
     </html>
   );
