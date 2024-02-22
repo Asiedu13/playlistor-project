a75dde16be93144408ca6fb925f46eb1014dd310
Prince Asiedu <princekofasiedu@gmail.com>
2024-02-22 17:15:49 +0000

    bug-fix: fixed missing key prop

--- app/components/common/results/ResultsMain.jsx
+++ app/components/common/results/ResultsMain.jsx
@@ -10,8 +10,8 @@ export function ResultsMain(links) {
       <div className="text-white text-lg">Search Results</div>
 
       <section className="grid grid-cols-4 gap-10 h-[700px] overflow-y-scroll overflow-x-hidden">
-        {results?.data.map((video) => (
-          <div className="text-white w-[300px] h-max rounded-md p-5 border-1 shadow-md bg-slate-400">
+        {results?.data.map((video, index) => (
+          <div key={index} className="text-white w-[300px] h-max rounded-md p-5 border-1 shadow-md bg-slate-400">
             <h2>{video.videoTitle}</h2>
             <a
               className="underline"
