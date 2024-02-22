4f0f1273e0142acc0a0457928e4e29c5d0cd4d8b
Prince Asiedu <princekofasiedu@gmail.com>
2024-02-22 09:15:58 +0000

    feature: setup page with new design

---
+++ app/components/common/results/ResultsMain.jsx
@@ -0,0 +1,8 @@
+export function ResultsMain() {
+    return (
+        <section>
+            {/* This is going to have the children components that contain the URLS and stuff */}
+            
+        </section>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,20 +1,29 @@
-import Link from "next/link";
-import { ListPlus } from "lucide-react";
+import { Youtube, Clapperboard } from "lucide-react";
 import { Logo } from "./components/common/ui/Logo";
+import { SearchComponent } from "./components/common/search/SearchComponent";
+
 export default function Home() {
   return (
-    <section className="bg-black h-full">
-      <header className="px-[40px] bg-black">
-        <nav className="h-[80px] py-8 flex items-center gap-5">
+    <section className="bg-background h-full">
+      <header className="mx-24 .my-[30px] bg-secondary rounded-md">
+        <nav className="h-[40px] py-8 flex items-center gap-5">
           <div className="w-[80px] h-[70px] ">
             <Logo />
           </div>
-          <button className="text-white text-sm font-medium flex items-center px-2 gap-2 w-[130px] h-[30px] bg-blue-500 rounded-md ">
-            <ListPlus />
-            My Playlists
+          <button className="text-white text-sm font-medium flex items-center px-2 gap-2 h-[30px] w-max .bg-blue-500 rounded-md ">
+            <Clapperboard size={20} strokeWidth={1} />
+            Service2
+          </button>
+          <button className="text-white text-sm font-medium flex items-center px-2 gap-2 h-[30px] w-max bg-blue-500 rounded-md ">
+            <Youtube size={20} strokeWidth={1} />
+            YouTube
           </button>
         </nav>
       </header>
+
+      <main className="mx-[100px] h-[300px] rounded-md .grid .grid-cols-2">
+        <SearchComponent />
+      </main>
     </section>
   );
 }
