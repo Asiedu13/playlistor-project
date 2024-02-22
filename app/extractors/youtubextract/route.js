aa3063fb7b45d142c252152ef6cf619ddeae0e2e
Prince Asiedu <princekofasiedu@gmail.com>
2024-02-22 11:52:03 +0000

    feature: youtube page extraction Youtube

---
+++ app/extractors/youtubextract/route.js
@@ -0,0 +1,41 @@
+import puppeteer from "puppeteer";
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const url = searchParams.get("url"); 
+
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+    });
+      const page = await browser.newPage( );
+    await page.goto(
+      url
+    
+    );
+
+    const links = await page.evaluate(() => {
+      const linkList = document.querySelectorAll("#content");
+
+      return Array.from(linkList).map((link) => {
+        const videoTitle = link.querySelector("h3").getAttribute('aria-label');
+        const videoLink = link
+          .querySelector("#video-title")
+          .getAttribute("href");
+
+        return { videoTitle, videoLink };
+      });
+    });
+
+    console.log(links);
+    await browser.close();
+    return NextResponse.json({
+      data: links
+    });
+  } catch (err) {
+    return NextResponse.json({
+      err,
+    });
+  }
+}
