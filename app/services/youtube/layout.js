e74bf676ec0d59e8c47a965cebc2e99d97675a1a
Prince Asiedu <princekofasiedu@gmail.com>
2024-02-22 09:13:41 +0000

    disabled page

--- app/services/youtube/layout.js
+++ app/services/youtube/layout.js
@@ -1,21 +1,21 @@
-import { Inter } from "next/font/google";
-import { NavBar } from "@/app/components/common/ui/Nav";
-import { Footer } from "@/app/components/common/ui/Footer";
+// import { Inter } from "next/font/google";
+// import { NavBar } from "@/app/components/common/ui/Nav";
+// import { Footer } from "@/app/components/common/ui/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "PlayListor | YouTube",
-  description: "Get all playlist Links from various providers",
-};
+// export const metadata = {
+//   title: "PlayListor | YouTube",
+//   description: "Get all playlist Links from various providers",
+// };
 
 
-export default function Layout({ children }) {
-  return (
-    <main className={inter.className}>
-      <NavBar />
-      {children}
-      <Footer />
-    </main>
-  );
-}
+// export default function Layout({ children }) {
+//   return (
+//     <main className={inter.className}>
+//       <NavBar />
+//       {children}
+//       <Footer />
+//     </main>
+//   );
+// }
