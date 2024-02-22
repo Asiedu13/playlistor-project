1b096736fc71a8abc8444ce36647ea61ad30f489
Prince Asiedu <princekofasiedu@gmail.com>
2024-02-22 16:58:15 +0000

    feature: display results

--- app/components/common/results/ResultsMain.jsx
+++ app/components/common/results/ResultsMain.jsx
@@ -1,8 +1,28 @@
-export function ResultsMain() {
-    return (
-        <section>
-            {/* This is going to have the children components that contain the URLS and stuff */}
-            
-        </section>
-    )
+"use client";
+import { SearchResults } from "../context/SearchResponseContext";
+import { useContext } from "react";
+
+export function ResultsMain(links) {
+  const { results, setResults } = useContext(SearchResults);
+  console.log(results?.data);
+  return (
+    <section>
+      <div className="text-white text-lg">Search Results</div>
+
+      <section className="grid grid-cols-4 gap-10 h-[700px] overflow-y-scroll overflow-x-hidden">
+        {results?.data.map((video) => (
+          <div className="text-white w-[300px] h-max rounded-md p-5 border-1 shadow-md bg-slate-400">
+            <h2>{video.videoTitle}</h2>
+            <a
+              className="underline"
+              target="_blank noreferrer noopener"
+              href={`https://youtube.com${video.videoLink}`}
+            >
+              Watch here
+            </a>
+          </div>
+        ))}
+      </section>
+    </section>
+  );
 }
